3c751e776d3f3a4a0fcbc0aea35556285d0d4c35
Christopher Ackerman <christopher.ackerman@gmail.com>
2024-06-15 14:01:16 -0400

    cleanup

--- enhanced_hooking.py
+++ enhanced_hooking.py
@@ -1,8 +1,10 @@
 ### Flexible hooking at arbitrary layers and tokens
+
 import torch
 import torch.nn as nn
 from collections import defaultdict
 
+# Read activations during model forward pass (training)
 def attach_activation_hooks(model, layers_positions, activation_storage, get_at='end'):
     """
     Attach hooks to specified layers to capture activations at specified positions.
@@ -10,18 +12,13 @@ def attach_activation_hooks(model, layers_positions, activation_storage, get_at=
     def capture_activations_hook(layer_idx, positions, get_at='end'):
         def hook(module, input, output):
             # output shape is (batch_size, sequence_length, hidden_size)
+            # positions a batch_size list of lists of position lists
             if isinstance(output, tuple): output = output[0]
-###            for i, pos_list in enumerate(positions):
-###                activation_storage[layer_idx].extend(output[i, pos_list, :].detach().cpu())
             for batch_idx in range(len(positions)):
                 for pos_idx, seq_pos in enumerate(positions[batch_idx]):
                     ###print(f"Layer={layer_idx}, seq_pos={seq_pos}")
                     activation_storage[layer_idx][pos_idx].append(output[batch_idx, seq_pos, :])#.detach().cpu()
         def pre_hook(module, input):
-###            for i, pos_list in enumerate(positions):
-###                selected_activations = input[0][i, pos_list, :]
-                #print(f"Type of selected_activations: {type(selected_activations)}, size: {selected_activations.size()}")
-###                activation_storage[layer_idx].extend(selected_activations.detach().cpu())
             for batch_idx in range(len(positions)):
                 for pos_idx, seq_pos in enumerate(positions[batch_idx]):
                     activation_storage[layer_idx][pos_idx].append(input[0][batch_idx, seq_pos, :])#.detach().cpu()
@@ -44,7 +41,7 @@ def get_activations(model, tokens, layers_positions, get_at='end'):
     Get activations from specific layers and positions.
     """
     # Prepare storage for activations
-    activation_storage = defaultdict(lambda: defaultdict(list))###defaultdict(list)
+    activation_storage = defaultdict(lambda: defaultdict(list))
 
     # Attach hooks to the model
     attach_activation_hooks(model, layers_positions, activation_storage, get_at)
@@ -61,7 +58,6 @@ def get_activations(model, tokens, layers_positions, get_at='end'):
         if get_at == 'end': block._forward_hooks.clear()
         else: block._forward_pre_hooks.clear()
 
-###    return dict(activation_storage)
     return {
         layer: {
             pos: torch.stack([tensor.detach().cpu() for tensor in tensors])
@@ -69,8 +65,9 @@ def get_activations(model, tokens, layers_positions, get_at='end'):
         }
         for layer, pos_dict in activation_storage.items()
     }
- 
+######## 
 
+# Add to activations during generation
 def create_add_activations_hook(layers_activations):
     """
     Create a hook to add activation vectors at specified positions within specified layers.
@@ -123,54 +120,31 @@ def create_add_activations_pre_hook(layers_activations):
         
         return inputs
     return hook
-
     
-def create_continuous_zeroout_activations_hook(continuouspos_layer_activations):
-    """
-    Create a hook to zero-out projections of vectors within specified layers with each new token generated.
-    """
-    def hook(module, inputs, outputs):
-        current_layer_idx = getattr(module, 'layer_idx', None)
-        if current_layer_idx in continuouspos_layer_activations:
-            direction_vector = continuouspos_layer_activations[current_layer_idx]
-            main_tensor = outputs[0] if isinstance(outputs, tuple) else outputs
-            
-            direction_vector = direction_vector.to(main_tensor.dtype).view(-1, 1)  # Shape [d_embed, 1]
-            projection = (main_tensor @ direction_vector) / torch.norm(direction_vector)**2
-            direction_vector = direction_vector.view(1, 1, -1)  # Reshape for broadcasting; add dummy dimensions for batch and seq while keeping the last the same
-            main_tensor -= (projection * direction_vector)
-#            direction_vector = activation_vector.view(1, 1, -1) # Reshape for broadcasting; add dummy dimensions for batch and seq while keeping the last the same
-#            projection = torch.sum(main_tensor * direction_vector, dim=2, keepdim=True) / torch.norm(direction_vector)**2
-#            main_tensor -= (projection * direction_vector)
-#            for position in range(main_tensor.shape[1]):
-#                projection = torch.dot(main_tensor[:, position, :], activation_vector) / torch.dot(activation_vector, activation_vector)
-#                main_tensor[:, position, :] -= (projection * activation_vector)
-        return (main_tensor,) + outputs[1:] if isinstance(outputs, tuple) else main_tensor
-    return hook
-
 
 def create_continuous_activation_hook(continuouspos_layer_activations):
+    """
+    Add to each new token as it passes through the model during generation.
+    """
     def hook(module, inputs, outputs):
         current_layer_idx = getattr(module, 'layer_idx', None)
         if current_layer_idx in continuouspos_layer_activations:
             activation_vector = continuouspos_layer_activations[current_layer_idx]
             main_tensor = outputs[0] if isinstance(outputs, tuple) else outputs
-            # Always add to the last position in the sequence
-            #print(f"outputs.size={main_tensor.size()}, activation_vector.size={activation_vector.size()}")
-            #print(f"Adding continuous activations at layer {current_layer_idx} at position {main_tensor.size(1) - 1} to value {main_tensor[:, main_tensor.size(1) - 1, :5]}")
-#            main_tensor[:, main_tensor.size(1) - 1, :] += activation_vector
             main_tensor += activation_vector #add to every token in the prompt at the first pass, and then adds to each new token (takes a long time to run with use_cache=False, since then it adds to whole prompt at every pass)
         return (main_tensor,) + outputs[1:] if isinstance(outputs, tuple) else main_tensor
     return hook
 
 
 def create_continuous_activation_pre_hook(continuous_layers_activations):
+    """
+    Add to each new token as it passes through the model during generation.
+    """
     def pre_hook(module, inputs):
         current_layer_idx = getattr(module, 'layer_idx', None)
         if current_layer_idx in continuous_layers_activations:
             activation_vector = continuous_layers_activations[current_layer_idx]
             inputs = list(inputs)  # Convert tuple to list for mutability
-#            inputs[0][:, inputs[0].size(1) - 1, :] += activation_vector
             inputs[0] += activation_vector
             inputs = tuple(inputs)  # Convert back to tuple to maintain integrity
         return inputs
@@ -214,6 +188,25 @@ def add_activations_and_generate(model, tokens, specificpos_layer_activations, c
             block._forward_pre_hooks.clear()
 
     return generated_ids
+########################
+
+# Zero out projections
+def create_continuous_zeroout_activations_hook(continuouspos_layer_activations):
+    """
+    Create a hook to zero-out projections of vectors within specified layers with each new token generated.
+    """
+    def hook(module, inputs, outputs):
+        current_layer_idx = getattr(module, 'layer_idx', None)
+        if current_layer_idx in continuouspos_layer_activations:
+            direction_vector = continuouspos_layer_activations[current_layer_idx]
+            main_tensor = outputs[0] if isinstance(outputs, tuple) else outputs
+            
+            direction_vector = direction_vector.to(main_tensor.dtype).view(-1, 1)  # Shape [d_embed, 1]
+            projection = (main_tensor @ direction_vector) / torch.norm(direction_vector)**2
+            direction_vector = direction_vector.view(1, 1, -1)  # Reshape for broadcasting; add dummy dimensions for batch and seq while keeping the last the same
+            main_tensor -= (projection * direction_vector)
+        return (main_tensor,) + outputs[1:] if isinstance(outputs, tuple) else main_tensor
+    return hook
 
 
 def zeroout_projections_and_generate(model, tokens, continuouspos_layer_activations, sampling_kwargs):
@@ -235,8 +228,9 @@ def zeroout_projections_and_generate(model, tokens, continuouspos_layer_activati
         block._forward_hooks.clear()
 
     return generated_ids
+##################
 
-
+# Read during generation
 def attach_generation_activation_hooks(model, layers_positions, activation_storage, get_at='end'):
     """
     Attach hooks to specified layers to capture activations at specified positions, then at final during generation.
@@ -291,11 +285,11 @@ def get_activations_and_generate(model, tokens, layers_positions, sampling_kwarg
         if get_at == 'end': block._forward_hooks.clear()
         else: block._forward_pre_hooks.clear()
 
-###    return dict(activation_storage)
     return {layer: {pos: torch.stack(tensors) for pos, tensors in pos_dict.items()} 
             for layer, pos_dict in activation_storage.items()}   
- 
+ ######################
 
+### Utilities to turn specific gradients on and off
 def disable_grad():
     def hook(grad):
         return None
@@ -313,12 +307,11 @@ def attach_zerograd_hooks(parameters):
 def remove_zerograd_hooks(handles):
     for handle in handles:
         handle.remove()
-
+###############################
 
 def get_blocks(model: nn.Module) -> nn.ModuleList:
     """ Get the ModuleList containing the transformer blocks from a model. """
     def numel_(mod):
-###        return sum(p.numel() for p in mod.parameters())
         if isinstance(mod, nn.Module):
             num_elements = sum(p.numel() for p in mod.parameters())
             return num_elements
